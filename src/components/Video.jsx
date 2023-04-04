9099c7b940e8c93ed5177e07b74936cf8289002e
Alexandra Chernova <estelelendil@mail.ru>
2023-04-04 11:36:42 +0300

    initial commit

---
+++ src/components/Video.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from 'react';
+import DateTimeWithWrapper from './myWrapper';
+
+export default function Video(props) {
+      return (
+        <div className="video">
+          <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <DateTimeWithWrapper date={props.date} />
+        </div>
+      )
+    }
